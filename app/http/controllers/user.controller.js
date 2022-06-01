278f3e8986a5b5bf2ee9d03b599097ffda91d18f
Nima <nimarahmati8par@gmail.com>
2022-06-01 14:11:11 +0430

    Upload Profile Photo✅

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -1,5 +1,5 @@
 const { UserModel } = require("../../models/user.model");
-
+const path = require("path")
 class UserController {
     GetProfile(req, res, next) {
         try {
@@ -13,7 +13,7 @@ class UserController {
     }
     async EditProfile(req, res, next) {
         try {
-            const data = {...req.body};
+            const data = { ...req.body };
             const userId = req.user._id;
             let fields = ["firstName", "lastName", "skills"]
             let badValues = ["", " ", ".", null, undefined, 0, -1];
@@ -22,11 +22,34 @@ class UserController {
                 if (badValues.includes(value)) delete data[key]
             })
             console.log(data);
-            const result = await UserModel.updateOne({_id: userId}, {$set: data})
-            if(result.modifiedCount > 0) return res.status(200).json({status:200, success: true, message: "Profile Was Updated Successfuly"})
-            throw {status: 400, success: false, message: "Profile Was Not Updated"}
+            const result = await UserModel.updateOne({ _id: userId }, { $set: data })
+            if (result.modifiedCount > 0) return res.status(200).json({ status: 200, success: true, message: "Profile Was Updated Successfuly" })
+            throw { status: 400, success: false, message: "Profile Was Not Updated" }
         } catch (error) {
             next(error);
+        }
+    }
+    async UploadProfile(req, res, next) {
+        try {
+            const userId = req.user._id;
+            const pathPrefix = path.join(__dirname, "..", "..");
+            console.log(req.file.path.substring(pathPrefix.length));
+            let image;
+            if (!req.file) {
+                throw { status: 400, success: false, message: "Please Choose A File" }
+            }
+            else {
+                image = req.file.path.substring(pathPrefix.length);
+            }
+            const result = await UserModel.updateOne({ _id: userId }, { $set: { profile: image } })
+            if (result.modifiedCount <= 0) {
+                throw { status: 400, success: false, message: "Nothing Changed In Databse" }
+            }
+            return res.json({
+                file: JSON.stringify(req.files)
+            })
+        } catch (error) {
+            next(error)
         }
     }
     AddSkills() {
--- app/routers/user.router.js
+++ app/routers/user.router.js
@@ -1,10 +1,12 @@
 const authController = require("../http/controllers/auth.controller");
 const {UserController} = require("../http/controllers/user.controller");
 const { checkLogin } = require("../http/middlewares/checkLogin");
+const { FileUploader } = require("../modules/utils");
 
 const userRouter = require("express").Router();
 
 userRouter.get("/profile", checkLogin, UserController.GetProfile)
 userRouter.post("/profile", checkLogin, UserController.EditProfile)
+userRouter.put("/profile/upload", checkLogin, FileUploader.single("profile"), UserController.UploadProfile)
 
 module.exports = userRouter
