ad8f1454eb6209a9708aad4a97ba2ba8b2c329db
Nima <nimarahmati8par@gmail.com>
2022-06-01 12:49:25 +0430

    Edit Profile And Skills✅

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -1,3 +1,5 @@
+const { UserModel } = require("../../models/user.model");
+
 class UserController {
     GetProfile(req, res, next) {
         try {
@@ -9,8 +11,23 @@ class UserController {
             next(error);
         }
     }
-    EditProfile() {
-
+    async EditProfile(req, res, next) {
+        try {
+            const data = {...req.body};
+            const userId = req.user._id;
+            let fields = ["firstName", "lastName", "skills"]
+            let badValues = ["", " ", ".", null, undefined, 0, -1];
+            Object.entries(data).forEach(([key, value]) => {
+                if (!fields.includes(key)) delete data[key]
+                if (badValues.includes(value)) delete data[key]
+            })
+            console.log(data);
+            const result = await UserModel.updateOne({_id: userId}, {$set: data})
+            if(result.modifiedCount > 0) return res.status(200).json({status:200, success: true, message: "Profile Was Updated Successfuly"})
+            throw {status: 400, success: false, message: "Profile Was Not Updated"}
+        } catch (error) {
+            next(error);
+        }
     }
     AddSkills() {
 
--- app/routers/user.router.js
+++ app/routers/user.router.js
@@ -5,5 +5,6 @@ const { checkLogin } = require("../http/middlewares/checkLogin");
 const userRouter = require("express").Router();
 
 userRouter.get("/profile", checkLogin, UserController.GetProfile)
+userRouter.post("/profile", checkLogin, UserController.EditProfile)
 
 module.exports = userRouter
